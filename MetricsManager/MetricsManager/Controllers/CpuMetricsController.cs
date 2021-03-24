1aba4be7361098c5eda3b742ced65c93c1f17f83
evgmolt <evg.molt@gmail.com>
2021-03-24 14:00:20 +0400

    Add Logger. Exception

--- MetricsManager/MetricsManager/Controllers/CpuMetricsController.cs
+++ MetricsManager/MetricsManager/Controllers/CpuMetricsController.cs
@@ -1,6 +1,7 @@
 ﻿using Enums;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,21 @@ namespace MetricsManager.Controllers
     [ApiController]
     public class CpuMetricsController : ControllerBase
     {
+        private readonly ILogger<CpuMetricsController> _logger;
+
+        public CpuMetricsController(ILogger<CpuMetricsController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog встроен в CpuMetricsController");
+        }
+
         [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsFromAgent(
             [FromRoute] int agentId, 
             [FromRoute] TimeSpan fromTime, 
             [FromRoute] TimeSpan toTime)
         {
+            _logger.LogInformation("Привет! Это наше первое сообщение в лог");
             return Ok();
         }
 
